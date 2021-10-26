2f2ea668e6b24c244b0a4bff546ab42771985a79
tai-cyan <taisukemar@icloud.com>
2021-10-27 06:33:05 +0900

    ihi

--- src/kernel/memmap.hpp
+++ src/kernel/memmap.hpp
@@ -10,3 +10,40 @@ struct MemMap {
   unsigned long long desc_s;
   uint32_t desc_ver; // 使わん
 };
+
+struct MemoryDescriptor{
+    uint32_t type;
+    uintptr_t physical_start;
+    uintptr_t virtual_start;
+    uint64_t number_of_pages;
+    uint64_t attribute;
+};
+
+#ifdef __cplusplus
+enum class MemoryType {
+  kEfiReservedMemoryType,
+  kEfiLoaderCode,
+  kEfiLoaderData,
+  kEfiBootServicesCode,
+  kEfiBootServicesData,
+  kEfiRuntimeServicesCode,
+  kEfiRuntimeServicesData,
+  kEfiConventionalMemory,
+  kEfiUnusableMemory,
+  kEfiACPIReclaimMemory,
+  kEfiACPIMemoryNVS,
+  kEfiMemoryMappedIO,
+  kEfiMemoryMappedIOPortSpace,
+  kEfiPalCode,
+  kEfiPersistentMemory,
+  kEfiMaxMemoryType
+};
+
+inline bool operator==(uint32_t lhs, MemoryType rhs) {
+  return lhs == static_cast<uint32_t>(rhs);
+}
+
+inline bool operator==(MemoryType lhs, uint32_t rhs) {
+  return rhs == lhs;
+}
+#endif

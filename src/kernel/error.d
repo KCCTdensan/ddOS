3ac0fb6cceef294109b34b78940871737729b75e
YamaD <me@dyama.net>
2021-10-27 16:41:30 +0900

    すっきり

--- src/kernel/error.d
+++ src/kernel/error.d
@@ -110,3 +110,47 @@ KError KernelError(KError.Code code_,
                    int line = __LINE__) {
   return KError(code_, __FILE__, __LINE__);
 }
+
+/* from hpp
+  static_assert(Code::EOC==CodeNames.size());
+
+public:
+  // kError(Code code_,const char* file_,int line_)
+  //   : code(code_),file(file_),line(line_){};
+  kError(Code code_) : code(code_){};
+
+  const char* Name() const {
+    return CodeNames[code];
+  }
+
+  Code Cause() const {
+    return this->code;
+  }
+  const char* File() const {
+    return this->file;
+  }
+  int Line() const {
+    return this->line;
+  }
+
+  operator bool() const {
+    return this->code!=kSuccess;
+  }
+private:
+  Code code;
+  const char* file;
+  int line;
+};
+
+template<class T>
+struct WithError {
+  T data;
+  kError error;
+};
+
+// std::source_locationが使えるようになるまで我慢
+// #define MAKE_ERROR(code) Error((code), __FILE__, __LINE__)
+inline kError KernelError(kError::Code code_){
+  return kError(code_);
+}
+*/

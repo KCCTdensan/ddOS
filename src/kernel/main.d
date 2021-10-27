591737f88df00ac12c8cd3634c173741c4a4cec3
YamaD <me@dyama.net>
2021-10-27 19:07:59 +0900

    微調整

--- src/kernel/main.d
+++ src/kernel/main.d
@@ -18,8 +18,8 @@ void KernelMainNewStack(ref const FBConf fbconf, ref const MemMap memmap) {
   // レイアウト的なの用意したい
   auto kFrameWidth = fbconf.res_horiz;
   auto kFrameHeight = fbconf.res_vert;
-  auto layout_single = fbconf.res_vert < 512;
-  auto layout_right_pane_width = 256;
+  auto layout_right_pane_width = 32+200+32; // 8px per pix
+  auto layout_single = fbconf.res_vert < layout_right_pane_width*2+2;
 
   // pixel_writer
   auto pixel_writer = PixelWriter(fbconf); // fbconfからPixelのフォーマットを察してくれる
@@ -32,8 +32,8 @@ void KernelMainNewStack(ref const FBConf fbconf, ref const MemMap memmap) {
         kFrameWidth-8, kFrameHeight-8, 4, 4,
         RGBColor(0,0,0), RGBColor(0x20,0xff,0x20))
     : KConsole(&pixel_writer,
-        kFrameWidth-layout_right_pane_width-8, kFrameHeight-8, 4, 4,
-        RGBColor(0,0,0), RGBColor(0xff,0xff,0xff));
+        kFrameWidth-layout_right_pane_width-8-2, kFrameHeight-8, 4, 4,
+        RGBColor(0,0,0), RGBColor(0x20,0xff,0x20));
 
   // temp 要置き換え
   void printk(string s) {
@@ -43,29 +43,23 @@ void KernelMainNewStack(ref const FBConf fbconf, ref const MemMap memmap) {
 
   // right info pane
   if(!layout_single) {
-    auto margin_left = kFrameWidth-layout_right_pane_width-8;
+    auto margin_left = kFrameWidth-layout_right_pane_width; // 2: border line
     (&pixel_writer).FillRectangle(
-        Vec2D(margin_left,0),
+        Vec2D(margin_left-2,0),
         Vec2D(2,kFrameHeight),
         RGBColor(0x40,0x40,0x40));
 
-    // QR (25 x 25)
-
-    bool [25][25] qrData = mixin(import("qr.txt")); // ~~/src/assets/qr.txt
-
-    // 余白
-    int padding_left = 30;
-
-    foreach(y; 0 .. 258)
-      foreach(x; 0 .. 258)
-        pixel_writer.write(margin_left+x, y, RGBColor(0xff,0xff,0xff));
-
+    // QR
+    bool[25][25] qrData = mixin(import("qr.txt")); // ~~/src/assets/qr.txt
+    foreach(y; 0 .. 264)
+      foreach(x; 0 .. 264)
+        pixel_writer.write(margin_left+x,y,RGBColor(0xff,0xff,0xff));
     foreach(y; 0 .. 25)
       foreach(x; 0 .. 25)
-        foreach(dy; 0 .. 8)
-          foreach(dx; 0 .. 8)
-            if(qrData[y][x])
-              pixel_writer.write(margin_left+x*8+dx+padding_left, y*8+dy+padding_left, RGBColor(0,0,0));
+        if(qrData[y][x])
+          foreach(dy; 0 .. 8)
+            foreach(dx; 0 .. 8)
+              pixel_writer.write(margin_left+32+8*x+dx,32+8*y+dy,RGBColor(0,0,0));
   }
 
   // showing log
